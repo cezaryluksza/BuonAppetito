43cf52b41e160c298f9d13ef1e12cc3627f53595
Cezary Łuksza <cezary.luksza@gmail.com>
2019-01-21 22:04:26 +0100

    init commit

---
+++ BuonAppetito/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuonAppetito.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu1()
+        {
+            ViewBag.Message = "Propozycja menu na imprezę plenerową";
+
+            return View();
+        }
+
+        public ActionResult Menu2()
+        {
+            ViewBag.Message = "Propozycja menu wielkanocnego";
+
+            return View();
+        }
+        public ActionResult Menu3()
+        {
+            ViewBag.Message = "Propozycja menu wigilijnego";
+
+            return View();
+        }
+    }
+}
